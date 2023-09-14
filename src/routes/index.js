b4ebf8e8259f79cdd509399f6694475b04dfdf34
celsocontim <celso.contim@hotmail.com>
2023-09-14 14:41:47 -0300

    Versao Swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,11 @@ import { Router } from 'express';
 import medicosRoutes from './medicos';
 
 const router = Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.use('/medicos', medicosRoutes);
 export default router;
